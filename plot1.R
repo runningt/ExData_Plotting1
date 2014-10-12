eb7ee97a8db6de8e25d6c76c9aee7ab522e6af07
Piotr Zawislak <pzawislak@spistresci.pl>
2014-10-12 23:20:10 +0200

    ExData_Plotting:  plot1 first try

--- plot1.R
+++ plot1.R
@@ -2,3 +2,5 @@ input = read.csv('../household_power_consumption.txt', sep=';', stringsAsFactor=
 library(dplyr)
 my_df = tbl_df(input)
 plot_days = filter(my_df,Date=='1/2/2007'|Date=='2/2/2007')
+
+hist(as.integer(plot_days$Global_active_power)*2, xlab='GlobalActivPower (kilowats)', main='Global Active Power')
