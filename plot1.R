a9f38f9fd1fd6334b78ceebe0150e53f8eeb8f0c
Piotr Zawislak <pzawislak@spistresci.pl>
2014-10-19 14:52:06 +0200

    ExData_Plotting:  plot1 done

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
-input = read.csv('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE)
+input = read.table('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE, na.strings='?')
 library(dplyr)
 my_df = tbl_df(input)
 plot_days = filter(my_df,Date=='1/2/2007'|Date=='2/2/2007')
-
-hist(as.integer(plot_days$Global_active_power)*2, xlab='GlobalActivPower (kilowats)', main='Global Active Power')
+png(file = "plot1.png", width = 480, height = 480)
+hist(as.numeric(plot_days$Global_active_power), xlab='GlobalActivPower (kilowats)', main='Global Active Power', col="red")
+dev.off()
