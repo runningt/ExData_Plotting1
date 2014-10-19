4ef275bc2b7eca443bd03597f38a4e3c99f61013
Piotr Zawislak <pzawislak@spistresci.pl>
2014-10-19 16:16:52 +0200

    ExData_Plotting:  plot3 and plot4 device fixed, plotting is still missing

--- plot4.R
+++ plot4.R
@@ -1,4 +1,9 @@
-input = read.csv('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE)
+input = read.table('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE, na.strings='?')
 library(dplyr)
 my_df = tbl_df(input)
 plot_days = filter(my_df,Date=='1/2/2007'|Date=='2/2/2007')
+
+png(file = "plot4.png", width = 480, height = 480)
+#plot(plot_days[3], type='l',ylab='GlobalActivPower (kilowats)', xlab='')
+
+dev.off()
