95db62954708c6e9ebd4b9005939a36aeffae4d8
Piotr Zawislak <pzawislak@spistresci.pl>
2014-10-19 16:05:47 +0200

    Some fixes in plot2, missing Date convertion

--- plot2.R
+++ plot2.R
@@ -1,7 +1,8 @@
-input = read.csv('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE)
+input = read.table('../household_power_consumption.txt', sep=';', stringsAsFactor=FALSE, na.strings='?')
 library(dplyr)
 my_df = tbl_df(input)
 plot_days = filter(my_df,Date=='1/2/2007'|Date=='2/2/2007')
+png(file = "plot2.png", width = 480, height = 480)
 plot(plot_days[3], type='l',ylab='GlobalActivPower (kilowats)', xlab='')
-
+dev.off()
 
